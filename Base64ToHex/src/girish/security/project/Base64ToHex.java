5c61a663ce4f27fc3a82c1978358e02a9d59ce49
Girish Raman <rgirish1994@gmail.com>
2015-10-31 15:58:32 -0700

    Done with Base64 to Hex

---
+++ Base64ToHex/src/girish/security/project/Base64ToHex.java
@@ -0,0 +1,43 @@
+package girish.security.project;
+
+public class Base64ToHex {
+
+	// At the end of the program, 'hex' will contain the output.
+	// hexLookUp is the lookup table for hex encoding of a bit sequence.
+	// I'm using the index value to reference it.
+	static String hex = "", hexLookUp = "0123456789abcdef",
+			base64LookUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";;
+
+	public static void main(String[] a) {
+
+		// Input Base64 String
+		String base64 = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+		System.out.println(base64.length());
+		for (int i = 0; i < base64.length(); ++i) {
+			// Take first 6 bits (from the first base64 digit) and call
+			// convert() with the first 4 bits.
+			String firstDigit = String.format("%06d",
+					Integer.parseInt(Integer.toBinaryString(base64LookUp.indexOf(base64.charAt(i++)))));
+			convert(firstDigit.substring(0, 4));
+
+			// Take the remaining 2 bits (from the first base64 digit) and the
+			// first 2 bits of the second base64 digit and call convert()
+			String secondDigit = String.format("%06d",
+					Integer.parseInt(Integer.toBinaryString(base64LookUp.indexOf(base64.charAt(i)))));
+			convert(firstDigit.substring(4, 6) + secondDigit.substring(0, 2));
+
+			convert(secondDigit.substring(2, 6));
+		}
+		System.out.println(hex);
+	}
+
+	/*
+	 * 'bits' will be a 4 bit sequence which will be converted to the
+	 * corresponding hex value. Each hex digit is appended to the string 'hex'.
+	 */
+	static void convert(String bits) {
+		StringBuilder stringBuilder = new StringBuilder(hex);
+		stringBuilder.append(String.valueOf(hexLookUp.charAt(Integer.parseInt(bits, 2))));
+		hex = stringBuilder.toString();
+	}
+}
