f86ad176811899d92dae736f96dfdec935fc2c6c
Girish Raman <rgirish1994@gmail.com>
2015-11-22 19:00:47 -0700

    Modified Padding to include all message sizes

--- Padding/src/padding/PKCS.java
+++ Padding/src/padding/PKCS.java
@@ -1,18 +1,20 @@
 package padding;
 
 public class PKCS {
-	public static void main(String[] args){
-		StringBuilder s = new StringBuilder("YELLOW SUBMARINE");
-		int bytes=4;
-		String s1=String.format("0x%02X", bytes);
-		/* @reference https://www.securecoding.cert.org/confluence/display/java/
-		 * EXP52-J.+Use+braces+for+the+body+of+an+if%2C+for%2C+or+while+statement
-		 * A for loop with one statement inside is enclosed within braces */
-		for(int i=0;i<bytes;i++)
-		{
-			s.append(s1);
+	public static void main(String[] args) {
+		StringBuilder s = new StringBuilder("shankar");
+		int blockSize = 16;
+		/*
+		 * @reference https://www.securecoding.cert.org/confluence/display/java/
+		 * EXP52-J.+Use+braces+for+the+body+of+an+if%2C+for%2C+or+while+
+		 * statement A for loop with one statement inside is enclosed within
+		 * braces
+		 */
+		int diff = (blockSize - s.length());
+		for (int i = 0; i < diff; i++) {
+			s.append(String.valueOf(diff));
 		}
-			System.out.println(s);
+		System.out.println(s);
 	}
 
 }
