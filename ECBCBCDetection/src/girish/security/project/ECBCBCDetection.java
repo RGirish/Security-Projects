cbda0eef68bca97de153ba978085fc7c57e67f02
Girish Raman <rgirish1994@gmail.com>
2015-11-22 22:20:46 -0700

    ECB CBC ecryption done with 0.5 probablitiy. Have to start with the detection part

--- ECBCBCDetection/src/girish/security/project/ECBCBCDetection.java
+++ ECBCBCDetection/src/girish/security/project/ECBCBCDetection.java
@@ -15,8 +15,29 @@ import javax.xml.bind.DatatypeConverter;
 public class ECBCBCDetection {
 
 	public static void main(String[] args) {
-		encryptionOracle("yellow submarine", generateRandomAESKey());
-		encryptionOracle("girish raman0202", generateRandomAESKey());
+		encryptionOracle("Girish", generateRandomAESKey());
+	}
+
+	private static String PKCSPadTheHexString(String hexString, int blockSize) {
+		StringBuilder s = new StringBuilder(hexString);
+		/*
+		 * EXP52-J. Use braces for the body of an if, for, or while statement
+		 * 
+		 * @reference https://www.securecoding.cert.org/confluence/display/java/
+		 * EXP52-J.+Use+braces+for+the+body+of+an+if%2C+for%2C+or+while+
+		 * statement A for loop with one statement inside is enclosed within
+		 * braces
+		 */
+		if (s.length() % 32 == 0) {
+			// No Padding
+		} else {
+			int diff = ((blockSize * 2) - (s.length() % 32)) / 2;
+			for (int i = 0; i < diff; i++) {
+				s.append("0");
+				s.append(Integer.toHexString((diff)));
+			}
+		}
+		return s.toString();
 	}
 
 	public static String generateRandomAESKey() {
@@ -49,15 +70,45 @@ public class ECBCBCDetection {
 			System.out.println("Key should be 16 Bytes (hex characters) long.");
 			return;
 		}
-
+		System.out.println("Hex Plain Text : " + asciiToHex(plainText));
+		System.out.println("Hex Key is : " + asciiToHex(key));
+		plainText = appendRandomPrefixAndSuffixToPlainText(asciiToHex(plainText));
+		System.out.println("PT after random padding is : " + plainText);
+		System.out.println("PT after PKCS padding is : " + PKCSPadTheHexString(plainText, 16));
+//		System.exit(0);
 		SecureRandom rand = new SecureRandom();
 		int bit = rand.nextInt(2);
 		if (bit == 0) {
-			System.out.println(encryptAESECB(asciiToHex(plainText), asciiToHex(key)));
+			System.out.println("***Encrypting in ECB Mode***");
+			System.out.println(encryptAESECB(PKCSPadTheHexString(plainText, 16), asciiToHex(key)));
 		} else {
-			// CBC
-			System.out.println(encryptAESECB(asciiToHex(plainText), asciiToHex(key)));
+			System.out.println("***Encrypting in CBC Mode***");
+			System.out.println(encryptAESCBC(PKCSPadTheHexString(plainText, 16), asciiToHex(key)));
 		}
+
+	}
+
+	/**
+	 * This function prefixes and suffixes 5-10 bytes to the plain text string.
+	 * 
+	 * @param plainText
+	 *            The plain text string in hex format
+	 * @return The plain text string with 5-10 bytes before and after
+	 */
+	private static String appendRandomPrefixAndSuffixToPlainText(String hexPlainText) {
+		SecureRandom rand = new SecureRandom();
+		int N = rand.nextInt(6) + 5;
+		StringBuilder builder = new StringBuilder();
+		String hexCharacters = "0123456789abcdef";
+		for (int i = 0; i < (N * 2); ++i) {
+			builder.append(hexCharacters.charAt(rand.nextInt(hexCharacters.length())));
+		}
+		builder.append(hexPlainText);
+		N = rand.nextInt(6) + 5;
+		for (int i = 0; i < (N * 2); ++i) {
+			builder.append(hexCharacters.charAt(rand.nextInt(hexCharacters.length())));
+		}
+		return builder.toString();
 	}
 
 	/**
@@ -87,6 +138,43 @@ public class ECBCBCDetection {
 		byte[] result = null;
 		try {
 			cipher = Cipher.getInstance("AES/ECB/NoPadding");
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			result = cipher.doFinal(DatatypeConverter.parseHexBinary(hexPlainText));
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("NoSuchAlgorithmException " + e.getMessage());
+			return null;
+		} catch (NoSuchPaddingException e) {
+			System.out.println("NoSuchPaddingException " + e.getMessage());
+			return null;
+		} catch (InvalidKeyException e) {
+			System.out.println("InvalidKeyException " + e.getMessage());
+			return null;
+		} catch (IllegalBlockSizeException e) {
+			System.out.println("IllegalBlockSizeException " + e.getMessage());
+			return null;
+		} catch (BadPaddingException e) {
+			System.out.println("BadPaddingException " + e.getMessage());
+			return null;
+		}
+		return new String(result);
+	}
+
+	/**
+	 * This function encrypts the given plain text in AES-CBC mode and returns
+	 * the cipher text.
+	 * 
+	 * @param hexPlainText
+	 *            The plain text in hex format
+	 * @param hexKey
+	 *            The random key in hex format
+	 * @return AES-CBC encryption of the plain text.
+	 */
+	private static String encryptAESCBC(String hexPlainText, String hexKey) {
+		SecretKey key = new SecretKeySpec(DatatypeConverter.parseHexBinary(hexKey), "AES");
+		Cipher cipher;
+		byte[] result = null;
+		try {
+			cipher = Cipher.getInstance("AES/CBC/NoPadding");
 			cipher.init(Cipher.ENCRYPT_MODE, key);
 			result = cipher.doFinal(DatatypeConverter.parseHexBinary(hexPlainText));
 		} catch (NoSuchAlgorithmException e) {
