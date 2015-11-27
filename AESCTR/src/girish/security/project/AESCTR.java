60ce2397f7786ef17b4ebe9b77d6ea0b931c3012
Girish Raman <rgirish1994@gmail.com>
2015-11-27 13:59:15 -0700

    done with AES CTR with custom nonce

---
+++ AESCTR/src/girish/security/project/AESCTR.java
@@ -0,0 +1,82 @@
+package girish.security.project;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AESCTR {
+
+	public static void main(String[] args) {
+		String asciiPlainText = "hey this is a long sentence that I'm writing so that it goes beyond 16 bytes!";
+		String asciiNonce = "enirambus wolley";
+		String asciiKey = "yellow submarine";
+
+		byte[] ctBytes = encryptAESCTR(asciiNonce, asciiPlainText, asciiKey);
+		byte[] ptBytes = decryptAESCTR(asciiNonce, ctBytes, asciiKey);
+
+		System.out.println(new String(ptBytes));
+	}
+
+	public static byte[] encryptAESCTR(String nonce, String plainText, String key) {
+		byte[] ptBytes = plainText.getBytes();
+		byte[] nonceBytes = nonce.getBytes();
+		byte[] temp = new byte[16];
+		byte[] ctBytes = new byte[plainText.length()];
+		int nonceCtrCount = 0;
+		int ctByteCount = 0;
+
+		for (int i = 0, j = 0; i < ptBytes.length; ++i) {
+			temp[j] = ptBytes[i];
+			++j;
+			if ((i + 1) % 16 == 0 || i + 1 == ptBytes.length) {
+				nonceBytes[15] = (byte) (nonceBytes[15] + nonceCtrCount);
+				nonceCtrCount++;
+				try {
+					SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
+					Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+					cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+					byte[] result = cipher.doFinal(nonceBytes);
+					for (int k = 0; k < result.length; ++k) {
+						ctBytes[ctByteCount] = (byte) (result[k] ^ temp[k]);
+						temp[k] = 0x00;
+						ctByteCount++;
+					}
+				} catch (Exception e) {
+				}
+				j = 0;
+			}
+		}
+		return ctBytes;
+	}
+
+	public static byte[] decryptAESCTR(String nonce, byte[] ptBytes, String key) {
+		byte[] nonceBytes = nonce.getBytes();
+		byte[] temp = new byte[16];
+		byte[] ctBytes = new byte[ptBytes.length];
+		int nonceCtrCount = 0;
+		int ctByteCount = 0;
+
+		for (int i = 0, j = 0; i < ptBytes.length; ++i) {
+			temp[j] = ptBytes[i];
+			++j;
+			if ((i + 1) % 16 == 0 || i + 1 == ptBytes.length) {
+				nonceBytes[15] = (byte) (nonceBytes[15] + nonceCtrCount);
+				nonceCtrCount++;
+				try {
+					SecretKey secretKey = new SecretKeySpec(key.getBytes(), "AES");
+					Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+					cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+					byte[] result = cipher.doFinal(nonceBytes);
+					for (int k = 0; k < result.length; ++k) {
+						ctBytes[ctByteCount] = (byte) (result[k] ^ temp[k]);
+						temp[k] = 0x00;
+						ctByteCount++;
+					}
+				} catch (Exception e) {
+				}
+				j = 0;
+			}
+		}
+		return ctBytes;
+	}
+}
--- AES_CTR/src/Aes_CTR/AES_CTR.java
+++ AES_CTR/src/Aes_CTR/AES_CTR.java
@@ -12,32 +12,29 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class AES_CTR {
-	public static String encrypt_aes_ctr(String pt , byte[] key,byte[] iv) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException{
+	public static byte[] encrypt_aes_ctr(String pt , byte[] key,byte[] iv) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException{
 		SecretKeySpec KEY = new SecretKeySpec(key , "AES");
 		IvParameterSpec ivspec = new IvParameterSpec(iv);
 		Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
 		cipher.init(Cipher.ENCRYPT_MODE,KEY,ivspec);
-		String ct = new String(cipher.doFinal(pt.getBytes()));
-		return ct;
+		return cipher.doFinal(pt.getBytes());
 		
 	}
-	public static String decrypt_aes_ctr(String ct , byte[] key , byte[] iv) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException{
+	public static byte[] decrypt_aes_ctr(byte[] ct , byte[] key , byte[] iv) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException{
 		SecretKeySpec KEY = new SecretKeySpec(key , "AES");
 		IvParameterSpec ivspec = new IvParameterSpec(iv);
 		Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
 		cipher.init(Cipher.DECRYPT_MODE,KEY,ivspec);
-		String pt = new String(cipher.doFinal(ct.getBytes()));	
-		return pt;
+		return cipher.doFinal(ct);
 	}
 
 	public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException {
 		byte[] key = new byte[] {0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x8,0x09,0x0A,0x0B,0x0C,0x0D,0x0E,0x0F};
 		byte[] iv = new byte[] {0x00,0x01,0x02,0x03,0x00,0x02,0x01,0x00,0x00,0x00,0x01,0x02,0x00,0x00,0x00,0x00};
-		String pt = "Hi this is Shankar" ;
-		String ct = encrypt_aes_ctr(pt,key,iv);
-		System.out.println("Cipher text : "+ct);
-		String pt1 = decrypt_aes_ctr(ct,key,iv);
-		System.out.println("Original plain text : "+pt1);
+		String pt = "This is shankar" ;
+		byte[] ct = encrypt_aes_ctr(pt,key,iv);
+		byte[] pt1 = decrypt_aes_ctr(ct,key,iv);
+		System.out.println("Original plain text : "+new String(pt1));
 	}
 
 }
