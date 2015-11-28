e5f8107586b12e39ec2862242444d7ab78b65c92
Girish Raman <rgirish1994@gmail.com>
2015-11-28 15:10:49 -0700

    small changes to SHA

--- SHA/src/sha/SHA.java
+++ SHA/src/sha/SHA.java
@@ -4,14 +4,27 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class SHA {
-	public static void main(String[] args) throws NoSuchAlgorithmException{
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
 		String input = "hello this is shankar";
-		MessageDigest md = MessageDigest.getInstance("SHA1");
-		byte[] result = md.digest(input.getBytes());
-		StringBuffer s = new StringBuffer();
-		for(int i=0;i<result.length;i++)
-			s.append(Integer.toString((result[i] & 0x0f)+0x0f , 16));
-		System.out.println("SHA 1output:"+s.toString());
+		String secretKey = "yellow submarine";
+		String toBeHashed = secretKey + input;
+		System.out.println("The MAC is: " + sha1(toBeHashed));
+	
+		input = "hello this is girish";
+		toBeHashed = secretKey + input;
+		System.out.println("The MAC is: " + sha1(toBeHashed));
 	}
+
+	static String sha1(String input) throws NoSuchAlgorithmException {
+        MessageDigest mDigest = MessageDigest.getInstance("SHA1");
+        byte[] result = mDigest.digest(input.getBytes());
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < result.length; i++) {
+            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+        }
+         
+        return sb.toString();
+    }
 
 }
