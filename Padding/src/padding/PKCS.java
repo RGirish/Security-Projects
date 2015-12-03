f04f5d5d98d5eb0d947e747e262c1b28db8efdb3
Shankar1112 <mskcoolguy93@gmail.com>
2015-12-02 21:21:24 -0700

    final-commit

--- Padding/src/padding/PKCS.java
+++ Padding/src/padding/PKCS.java
@@ -3,16 +3,21 @@ package padding;
 public class PKCS {
 	public static void main(String[] args) {
 		StringBuilder s = new StringBuilder("yellow submarine");
-		int blockSize = 16;
+		int blockSize = 20;
 		/*
 		 * @reference https://www.securecoding.cert.org/confluence/display/java/
 		 * EXP52-J.+Use+braces+for+the+body+of+an+if%2C+for%2C+or+while+
 		 * statement A for loop with one statement inside is enclosed within
 		 * braces
 		 */
-		int diff = (blockSize - s.length());
-		for (int i = 0; i < diff; i++) {
+		
+		//int diff = (blockSize - s.length());
+		int diff = blockSize-(  s.length()%blockSize);
+		if(diff!=blockSize)
+		{
+			for (int i = 0; i < diff ; i++) {
 			s.append(String.valueOf(diff));
+			}
 		}
 		System.out.println(s);
 	}
